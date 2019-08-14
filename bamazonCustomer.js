8d5684f4b5f4ce8e86433d934dad7eab8e15286b
Liz Guenthner <liz.guenthner@t-3.com>
2019-08-14 18:23:14 -0500

    add console.table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,6 +20,7 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
+  
   startBamazon();
 });
 
@@ -28,13 +29,20 @@ function startBamazon() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         console.log("--------------  BAMAZON  -------------\n");
-        for (var i = 0; i < res.length; i++) {
-          console.log(res[i].item_id + "  |  " + res[i].product_name + "  |  " + res[i].department_name + "  |  " + res[i].price + "  |  " + res[i].stock_quantity);
-        }
+        console.table(res);
+
+    //     for (var i = 0; i < res.length; i++) {
+    //       console.log(res[i].item_id + "  |  " + res[i].product_name + "  |  " + res[i].department_name + "  |  " + res[i].price + "  |  " + res[i].stock_quantity);
+    //     }
         console.log("\n-----------------------------------\r");
+
+        askQuestions(res);
+
       });
-    inquirer
-      var answers = [
+  }
+
+  function askQuestions(res) {
+    inquirer.prompt([
       {
           message: "What is the item id you would like to buy?",
           type: "input",
@@ -45,49 +53,20 @@ function startBamazon() {
           type: "input",
           name: "quantity"
         //   validate: validateName
-      },
+      }
       
-      ];
-      inquirer.prompt(answers)
-      .then(function(res) {
-        var query = "SELECT item_id, stock_quantity FROM products WHERE ?";
-        connection.query(query, { itemID: products.item_id, quantity: products.stock_quantity }, function(err, res) {
-            if (err) throw err;
-            for (var i = 0; i < res.length; i++) {
-                console.log("Item ID: " + res[i].item_id + " || Units: " + res[i].stock_quantity);
-          }
-        //   runSearch();
-        });
-      });
-  }
+      ])
+    .then(function(answer) {
 
-// let config,
-//   data,
-//   output;
- 
-// data = [
-//   ['0A', '0B', '0C'],
-//   ['1A', '1B', '1C'],
-//   ['2A', '2B', '2C']
-// ];
- 
-// config = {
-//   columns: {
-//     0: {
-//       alignment: 'left',
-//       width: 10
-//     },
-//     1: {
-//       alignment: 'center',
-//       width: 10
-//     },
-//     2: {
-//       alignment: 'right',
-//       width: 10
-//     }
-//   }
-// };
- 
-// output = table(data, config);
- 
-// console.log(output);
+      console.log(answer.quantity)
+      console.log(res);
+      // var query = "SELECT item_id, stock_quantity FROM products WHERE ?";
+      // connection.query(query, { itemID: res.item_id, quantity: products.stock_quantity }, function(err, res) {
+      //     if (err) throw err;
+      //     for (var i = 0; i < res.length; i++) {
+      //         console.log("Item ID: " + res[i].item_id + " || Units: " + res[i].stock_quantity);
+      //   }
+      // //   runSearch();
+      // });
+    });
+  }
